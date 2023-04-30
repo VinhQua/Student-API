42036af2a1f12980c6946ccd09f3ac3decb61599
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-04-30 22:29:19 +0900

    add securities

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ require('express-async-errors')
 
 
 //extra security packages
-
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
 
 
 //swagger
-
+const swagger = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 const express = require('express');
 const app = express();
@@ -22,16 +27,26 @@ const authenticateUser = require('./middles/authentication')
 
 //error handlers
 
-
-//extra packages
 const notFoundMiddleware = require('./middles/not-found')
 const errorMiddleware = require('./middles/error-handler');
 const errorHandlerMiddleware = require('./middles/error-handler');
 
+app.set('trust proxy',1);
+app.use(
+    rateLimiter({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+        standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+        legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    })
+)
 
 
 app.use(express.static('./client/build'))
 app.use(express.json())
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 //routes
 app.use('/api/v1/auth',authRouter)
 app.use('/api/v1/student',authenticateUser,studentRouter)
